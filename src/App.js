d8c6c38530a1f68a5a23b23b782fc5bbc332a93e
adamman722 <tarpeyadam@gmail.com>
2022-04-12 22:46:49 -0400

    added stuffff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
+import React, { useEffect } from "react";
+
 import "./App.css";
+import axios from "axios";
 
 function App() {
   //this is going to be a theme test see how it actually works
   //start 620pm
-  return <div className="App"></div>;
+  // really don't feel like it  damn I need to find my motivation
+  //I am slowly sinking more and more into my code and this is all I feel is right anymore
+  // token 51210047-2547-4fbc-b60f-b06f28647c7a
+  const grabTokens = async () => {
+    const url = "http://localhost:3000/api.coincap.io/v2/assets/bitcoin";
+    const config = {
+      headers: { Authorization: "Bearer 51210047-2547-4fbc-b60f-b06f28647c7a" },
+    };
+    axios.post(url, config).then(console.log);
+  };
+
+  useEffect(() => {
+    grabTokens();
+  }, []);
+
+  return <></>;
 }
 
 export default App;
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import CoinSliceReducer from "../features/CoinSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    CoinSlice: CoinSliceReducer,
   },
 });
